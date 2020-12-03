d3bedd7cea4504b57bdded95e9eab482d84011b6
Jean-François GAUTREAU <Jeff59550@gmail.com>
2020-12-03 18:47:49 +0100

    :sparkles: : add start files

---
+++ src/test/java/com/myCity/MyCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
